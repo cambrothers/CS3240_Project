4d9240075ad84007009b65cded47ca33cfc5ad26
Abigail Barber <agb5bt@virginia.edu>
2021-03-15 22:46:23 -0400

    commented out the first "import django-heroku" to see if travis will build

--- CS3240_A20/settings.py
+++ CS3240_A20/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+# import django_heroku   ---- commented out to see if travis wil build
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
