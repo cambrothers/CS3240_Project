ac6494e1657ef9bf3a4b84da7e4d53decbdc1730
Abigail Barber <agb5bt@virginia.edu>
2021-03-29 16:36:10 -0400

    Travis test5: test_runner = False

--- CS3240_A20/settings.py
+++ CS3240_A20/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIR = (
     #    django_heroku.settings(locals())
     # Configure Django App for Heroku.  commented out to see if above code works for travis build
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner = False)
 #except ImportError:
  #   found = False
 
