6952698bc342c780e95cf5555b44db181089ac76
JulietteLaburthe <jkl6et@virginia.edu>
2021-04-05 17:12:43 -0400

    Revert "merging the profile with main - database is still sqlite"
    This reverts commit 3c46834e2b284c44f5ea9124fa7795c6011be09a, reversing
    changes made to a59a94ecc5ea851d4ec6aab27df685a060716c86.

--- CS3240_A20/urls.py
+++ CS3240_A20/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from users import views as users_views
-from django.conf import settings 
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='CS3240_A20/index.html'),name='home'),
+    path('', TemplateView.as_view(template_name='CS3240_A20/index.html')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('profile/', users_views.profile, name='profile'),
-    
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
