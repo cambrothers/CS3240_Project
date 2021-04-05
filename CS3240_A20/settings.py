c1a517d7aaa383642f638b211b18ec087adb0a4a
JulietteLaburthe <jkl6et@virginia.edu>
2021-04-05 15:45:00 -0400

    added database

--- CS3240_A20/settings.py
+++ CS3240_A20/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'imagekit',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    
     'cloudinary',
     
 ]
@@ -87,8 +88,13 @@ WSGI_APPLICATION = 'CS3240_A20.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hnhcskjj',
+        'USER': 'hnhcskjj', 
+        'PASSWORD': 'DUwApclx7HeIOr71yaQo9qHDv7XZAVgC',
+        'HOST': 'queenie.db.elephantsql.com',
+        'PORT': '5432',
+        
     }
 }
 
