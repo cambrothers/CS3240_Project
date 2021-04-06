39edc9c4f57c2f1ed99c2018404686122554c39e
JulietteLaburthe <jkl6et@virginia.edu>
2021-04-06 08:30:10 -0400

    updating main to current working project

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class UserProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image','name','bio','pronouns','age','year','dorm_pref','school','roomates', 'schedule_image']
