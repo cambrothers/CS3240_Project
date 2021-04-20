a3e11fe439b192ee09055b089f1c386e41566c0c
Campbell Brothers <cb6cw@viriginia.edu>
2021-04-20 17:41:05 -0400

    added test_cases
    Rewriting the test cases I lost

--- tests.py
+++ tests.py
@@ -1,10 +1,41 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from users.models import Profile
 
-# Create your tests here.
+#def create_user =
 
-class DummyTestCase(TestCase):
-    def setUp(self):
-        x = 1
-    
-    def test_dummy_test_case(self):
-        self.assertEqual(1, 1)
+class ProfileCreate(TestCase):
+
+    def test_dummy(self):
+        self.assertTrue(True)
+#
+#     def test_is_username(self):
+#         User.objects.create_user(username= 'user')
+
+# class UserTest(unittest.TestCase):
+#     def test_setUp(self):
+#         self.user = User.objects.create_superuser(self.username, self.email, self.password)
+#         self.assertEqual(self.user.username, 'user')
+#         print(self.user.username)
+#         # self.assertTrue(self.user.first_name, 'name')
+#         # print(self.user.first_name)
+
+class ProfileTest(unittest.TestCase):
+    def test_required_name(self):
+        print("Before the no_name test")
+        # #no_name = Profile.objects.create(name='')
+        # name = Profile.name
+        #
+        # self.assertEqual(name, 'name')
+        print("You need to go back a place your name")
+
+class ViewTest(unittest.TestCase):
+    def test_setUp(self):
+        self.factory = RequestFactory
+        self.user = User.objects.create_user(username='name', email='user@example.com',  password='password')
+
+    # def test_details(self):
+    #     request = self.factory.get('/users/users/profile')
+    #     request.user = self.user
+
