7475d13167bdd720bd8d05bbc9930b24e7e66df2
JulietteLaburthe <jkl6et@virginia.edu>
2021-04-19 21:33:56 -0400

    adding friends

---
+++ users/friend_requests.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class FriendRequestStatus(Enum):
+    NO_REQUEST = -1
+    THEM_SENT_YOU =0
+    YOU_SENT_THEM =1
--- users/signals.py
+++ users/signals.py
@@ -1,12 +1,25 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile,Relation
 
 @receiver(post_save,sender=User)
 def create_profile(sender,instance,created,**kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+   if created:
+     Profile.objects.create(user=instance)
+
+@receiver(post_save,sender=Relation)
+def add_to_friends(sender,created,instance,**kwargs):
+    sender_ = instance.sender.user
+    print(sender_)
+    receiver_= instance.receiver.user
+    print(receiver_)
+    if instance.status == 'accepted':
+        print("hi")
+        sender_.friends.add(receiver_)
+        receiver_.friends.add(sender_)
+        sender_.save()
+        receiver_.save()
 
 @receiver(post_save,sender=User)
 def save_profile(sender,instance,**kwargs):
