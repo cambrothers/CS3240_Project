96d46d56eb0dcc855629c82ffcc1bb90055d42ca
Campbell Brothers <cb6cw@viriginia.edu>
2021-04-20 17:54:17 -0400

    new test cases

--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ from users.models import Profile
 #def create_user =
 
 class ProfileCreate(TestCase):
-
+    """
+    Quick test to see if the assert is working
+    """
     def test_dummy(self):
         self.assertTrue(True)
 #
@@ -14,17 +16,23 @@ class ProfileCreate(TestCase):
 #         User.objects.create_user(username= 'user')
 
 # class UserTest(unittest.TestCase):
+        """
+        Test to see if username works correctly
+        """
 #     def test_setUp(self):
-#         self.user = User.objects.create_superuser(self.username, self.email, self.password)
-#         self.assertEqual(self.user.username, 'user')
-#         print(self.user.username)
-#         # self.assertTrue(self.user.first_name, 'name')
+        self.user = User.objects.create_user(username='name', email='user@example.com',  password='password')
+        self.assertEqual(self.user.username, 'name')
+        print(self.user.username)
+        # self.assertEqual(self.user.first_name, 'name')
 #         # print(self.user.first_name)
 
 class ProfileTest(unittest.TestCase):
     def test_required_name(self):
+        blank = ''
         print("Before the no_name test")
-        # #no_name = Profile.objects.create(name='')
+        self.name = Profile(name = 'name')
+        # print(self.Profile.name)
+        self.assertEqual(self.name, blank)
         # name = Profile.name
         #
         # self.assertEqual(name, 'name')
@@ -39,5 +47,8 @@ class ViewTest(unittest.TestCase):
     #     request = self.factory.get('/users/users/profile')
     #     request.user = self.user
 
-class ScheduleTest(unittest.TestCase)
+class ScheduleTest(unittest.TestCase):
+    """
+    Testing to make sure the image appears
+    """
 
