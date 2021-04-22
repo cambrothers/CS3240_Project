89f398610ff8727de69cd6833ff1a4e48ba26fd1
JulietteLaburthe <77637917+JulietteLaburthe@users.noreply.github.com>
2021-04-22 11:16:10 -0400

    Update settings.py

--- CS3240_A20/settings.py
+++ CS3240_A20/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w&qb8&2)v+9er899jx+@5kkvz*__6yt2)u1ddgz+&!ag=3)^^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
